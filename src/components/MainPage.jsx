ca026e19b40d9456c6908c23c2eac9c73815211c
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-10-17 20:44:53 -0400

    Refactor import statement in MainPage component

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const MainPage = ({ supabase }) => {
   const [currentDate, setCurrentDate] = useState(new Date());
